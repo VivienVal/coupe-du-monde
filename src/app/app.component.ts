c0d42f40fdbbcdd1ef85f27850a56a176946845b
VALENCOURT <vivien.valencourt@isen.yncrea.fr>
2018-05-21 21:30:42 +0200

    authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	constructor(){
+		var config = {
+		    apiKey: "AIzaSyArbcTmfifsGZBpA0nvalxQE49aZ1spToo",
+		    authDomain: "coupe-du-monde-47bea.firebaseapp.com",
+		    databaseURL: "https://coupe-du-monde-47bea.firebaseio.com",
+		    projectId: "coupe-du-monde-47bea",
+		    storageBucket: "",
+		    messagingSenderId: "44202091389"
+	    };
+	    firebase.initializeApp(config);
+	}
 }
