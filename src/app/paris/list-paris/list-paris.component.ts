da201fa5ae26bc0a3e4138db78138aac06e2f4fa
VALENCOURT <vivien.valencourt@isen.yncrea.fr>
2018-06-04 17:10:59 +0200

    list pari to single match

--- src/app/paris/list-paris/list-paris.component.ts
+++ src/app/paris/list-paris/list-paris.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Pari } from '../../models/pari.model';
+import { Match } from '../../models/match.model';
 import { ParisService } from '../../services/paris.service';
 import { Subscription } from 'rxjs/subscription';
 import { AuthService } from '../../services/auth.service';
+import { MatchsService } from '../../services/matchs.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-paris',
@@ -12,11 +15,15 @@ import { AuthService } from '../../services/auth.service';
 export class ListParisComponent implements OnInit, OnDestroy {
 
   paris: Pari[];
+  matchs: Match[];
   parisSubscription: Subscription;
+  matchsSubscription: Subscription;
   options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
 
   constructor(  private parisService: ParisService,
-                private authService: AuthService) { }
+                private authService: AuthService,
+                private matchsService: MatchsService,
+                private router: Router) { }
 
   ngOnInit() {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -25,7 +32,21 @@ export class ListParisComponent implements OnInit, OnDestroy {
 	  			this.paris = paris;
 	  		}
 	  );
+    this.matchsSubscription = this.matchsService.matchsSubject.subscribe(
+        (matchs: Match[]) => {
+          this.matchs = matchs;
+        }
+    );
+    this.matchsService.emitMatchs();
 	  this.parisService.emitParis();
+  }
+
+  onViewPari(index: number){
+    for (let i in this.matchs){
+      if (this.matchsService.matchEquals(this.matchs[i], this.paris[index].match)){
+        return this.router.navigate(['/matchs', 'view', i]);
+      }
+    }
   }
 
   ngOnDestroy(){
